b2353f372155e79bd135590dfb835fbdc64015d9
EuReinoso <lucas.reinoso.br@gmail.com>
2021-04-21 20:44:27 -0300

    grid list and init nodes

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ class Node:
 
 WINDOW_SIZE = (640, 480)
 
+tile_size = 20
+grid_list = np.empty((WINDOW_SIZE[0]//tile_size, WINDOW_SIZE[1]//tile_size), dtype= object)
+
+def init_nodes():
+    for i in range(len(grid_list)):
+        for j in range(len(grid_list[0])):
+            rect = pygame.Rect(i * tile_size, j * tile_size, tile_size, tile_size)
+            grid_list[i][j] = Node(rect)
 
 window = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption('Maze')
