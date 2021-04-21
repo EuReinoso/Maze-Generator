58cee7ec08cdac2ef6949a646fdd0217020599be
EuReinoso <lucas.reinoso.br@gmail.com>
2021-04-21 20:54:50 -0300

    class Edged and init_edges

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ pygame.init()
 class Node:
     def __init__(self, rect):
         self.rect = rect
-        self.adjacents = []
+        self.edges = []
+        self.visited = False
+
+class Edge:
+    def __init__(self, node):
+        self.node = node
+        
 
 WINDOW_SIZE = (640, 480)
 
@@ -19,10 +25,23 @@ def init_nodes():
             rect = pygame.Rect(i * tile_size, j * tile_size, tile_size, tile_size)
             grid_list[i][j] = Node(rect)
 
+def init_edges():
+    for i in range(len(grid_list)):
+        for j in range(len(grid_list[0])):
+            if i > 0:
+                grid_list[i][j].edges.append(Edge(grid_list[i - 1][j]))
+            if i < len(grid_list) - 1:
+                grid_list[i][j].edges.append(Edge(grid_list[i + 1][j]))
+            if j > 0:
+                grid_list[i][j].edges.append(Edge(grid_list[i][j - 1]))
+            if j < len(grid_list[0]) - 1:
+                grid_list[i][j].edges.append(Edge(grid_list[i][j + 1]))
+
 window = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption('Maze')
 
 init_nodes()
+init_edges()
 while True:
 
     window.fill((0, 0, 0))
