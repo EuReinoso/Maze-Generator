7ea7c1badff5d1fb6b9f7b0bbc2203c544efb537
EuReinoso <lucas.reinoso.br@gmail.com>
2021-04-21 20:42:14 -0300

    class Node

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import pygame, sys
+import numpy as np
 
 pygame.init()
 
+class Node:
+    def __init__(self, rect):
+        self.rect = rect
+        self.adjacents = []
+
 WINDOW_SIZE = (640, 480)
+
 
 window = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption('Maze')
 
+init_nodes()
 while True:
 
     window.fill((0, 0, 0))
