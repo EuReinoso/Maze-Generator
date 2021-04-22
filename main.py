fc488c49382d217e9056eac25c840c3e380db186
EuReinoso <lucas.reinoso.br@gmail.com>
2021-04-22 09:40:18 -0300

    settings directions

--- main.py
+++ main.py
@@ -7,11 +7,21 @@ pygame.init()
 class Node:
     def __init__(self, rect):
         self.rect = rect
-        self.edges = []
+        self.edges = {}
         self.visited = False
 
+        self.walls = {'top' : True, 'down' : True, 'left': True, 'right' : True}
+        
     
-
+    def oposite_direction(self, direction):
+        if direction == 'top':
+            return 'down'
+        if direction == 'down':
+            return 'top'
+        if direction == 'left':
+            return 'right'
+        if direction == 'right':
+            return 'left'
 class Edge:
     def __init__(self, node):
         self.node = node
@@ -32,20 +42,31 @@ def init_edges():
     for i in range(len(grid_list)):
         for j in range(len(grid_list[0])):
             if i > 0:
-                grid_list[i][j].edges.append(Edge(grid_list[i - 1][j]))
+                grid_list[i][j].edges['left'] = Edge(grid_list[i - 1][j])
             if i < len(grid_list) - 1:
-                grid_list[i][j].edges.append(Edge(grid_list[i + 1][j]))
+                grid_list[i][j].edges['right'] = Edge(grid_list[i + 1][j])
             if j > 0:
-                grid_list[i][j].edges.append(Edge(grid_list[i][j - 1]))
+                grid_list[i][j].edges['top'] = Edge(grid_list[i][j - 1])
             if j < len(grid_list[0]) - 1:
-                grid_list[i][j].edges.append(Edge(grid_list[i][j + 1]))
+                grid_list[i][j].edges['down'] = Edge(grid_list[i][j + 1])
 
 def draw():
     for i in range(len(grid_list)):
         for j in range(len(grid_list[0])):
             if grid_list[i][j].visited == True:
-                pygame.draw.rect(window, (200, 200, 200), grid_list[i][j].rect)
+                pygame.draw.rect(window, (50, 50, 200), grid_list[i][j].rect)
 
+                if grid_list[i][j].walls['top']:
+                    pygame.draw.line(window, (200,200,200), grid_list[i][j].rect.topleft, grid_list[i][j].rect.topright, width=2)
+                if grid_list[i][j].walls['down']:
+                    pygame.draw.line(window, (200,200,200), grid_list[i][j].rect.bottomleft, grid_list[i][j].rect.bottomright, width=2)
+                if grid_list[i][j].walls['left']:
+                    pygame.draw.line(window, (200,200,200), grid_list[i][j].rect.topleft, grid_list[i][j].rect.bottomleft, width=2)
+                if grid_list[i][j].walls['right']:
+                    pygame.draw.line(window, (200,200,200), grid_list[i][j].rect.topright, grid_list[i][j].rect.bottomright, width=2)
+
+                pygame.draw.rect(window, (200,0,0), re.actual.rect)
+                
 window = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption('Maze')
 
@@ -56,7 +77,7 @@ init_edges()
 
 re = Recursive(grid_list[0][0])
 
-fps= 30
+fps= 3
 time = pygame.time.Clock()
 while True:
     
@@ -67,7 +88,7 @@ while True:
             pygame.quit()
             sys.exit()
 
-    draw()
     re.generate(window)
+    draw()
     pygame.display.update()
     time.tick(fps)
